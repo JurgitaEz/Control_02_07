34ad7ebb7b832acf6dc4741300fa01b456f3ab9a
unknown <jurgita.rud@gmail.comm>
2023-02-12 22:08:39 +0200

    final trye

--- allRecepies.js
+++ allRecepies.js
@@ -1,2 +1,35 @@
 
-//antrame HTML reikia i recepes card ideti recepto kortele
+const inputs = document.querySelectorAll("input")
+const filterBtn = document.querySelector("button")
+const recipesCards = document.querySelector(".recipesCards")
+
+let recipes = [];
+
+function getRecipes () {
+    fetch("https://www.themealdb.com/api/json/v1/1/random.php")
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+
+            recipes = data.recipes
+
+            appendRecipesCards(data.recipes)
+        })
+}
+getRecipes()
+
+function appendRecipesCards (data) {
+        data.map (item => {
+            recipesCards.innerHTML +=
+                    <div class="box1 d-flex fd-column ai-center jc-center">
+                        <h3>Title: ${item.strMeal}</h3>
+                        <img src ="${item.strMealThumb}" alt="">
+                        <h4>List of Ingrediens: ${item.strIngredient1}</h4>
+                        <h4>Description: ${item.strInstructions} </h4>
+                    </div>
+        })
+}
+appendRecipesCards()
+        //kodel man siulo className ??
+        //kodel meta klaida "Unecpected token "<" 24 eiluteje
+
--- index.js
+++ index.js
@@ -2,9 +2,17 @@
 const button = document.querySelectorAll("button")
 const inputs = document.querySelectorAll("input")
 const imgBox = document.querySelector("img")
+const recepesCards = document.querySelector(".recepesCards")
+
+const previewTitle = document.getElementById("previewTitle")
+const previewCalories = document.getElementById("previewCalories")
+const listOfIngrediens = document.querySelector("h4")
+const previewDescription = document.querySelector("p")
 
 let meals = [];
 let images = [];
+let instructions = [];
+
 
 function getMeal () {
     fetch("https://www.themealdb.com/api/json/v1/1/random.php")
@@ -14,9 +22,12 @@ function getMeal () {
             meals = data.meals
 
             const image = data.meals[0].strMealThumb
-            images = image
+            images.push (image)
             console.log(image)
-            appendImgBox(data)
+
+            const instruction = data.meals[0].strInstructions
+            instructions.push (instruction)
+            console.log(instruction)
         })
 }
 getMeal()
@@ -25,19 +36,25 @@ button[0].onclick = () => {
    console.log("prideda ingridientus")
 }
 
-function appendImgBox (data) {
-    imgBox.innerHTML = images
-}
 button[1].onclick = () => {
-    getMeal()
-    appendImgBox()
-    console.log("prideda nuotrauka")
+   console.log("prideda nuotrauka")
 }
 
 button[2].onclick = () => {
     console.log("prideda i preview")
+
+    previewTitle.innerHTML = "Title: " + inputs[0].value
+    listOfIngrediens.innerHTML = "List of ingrediens " + inputs[1].value
+    previewCalories.innerHTML = "Calories: " + inputs[2].value
+    previewDescription.innerHTML = "Descriptio: " + instructions
+    imgBox.src = images
+
+    inputs[0].value = null
+    inputs[1].value = null
+    inputs[2].value = null
 }
 
 button[3].onclick = () => {
     console.log("prideda i List")
+    //kaip prideti, kad nukeliautu i kita puslapi?
 }
